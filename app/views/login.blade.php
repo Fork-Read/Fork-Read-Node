95b5803a0a51440231700ae2072b2720c7c65920
Prateek Agarwal <prateekagr98@gmail.com>
2014-07-02 23:07:33 +0530

    Added the footer

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -8,10 +8,30 @@
 	</head>
 	<body>
 		<div class="login-form-container">
-			<input type="text" name="username" placeholder="enter email.." />
+			<input type="text" name="username" placeholder="enter email.." autofocus/>
 			<input type="password" name="password" placeholder="enter password.." />
 			<button type="submit" class="btn login-button">Login</button>
 			<button type="button" class="btn signup-button" onclick="location.href = '/signup';">Signup</button>
 		</div>
+
+		<footer>
+			<ul class="footerMenu">
+				<li>
+					<a href="#">about us</a>
+				</li>
+				<li>
+					<a href="#">contact us</a>
+				</li>
+				<li>
+					<a href="#">Copyright &copy; 2014 Fork&Read</a>
+				</li>
+				<li>
+					<a href="#">blog</a>
+				</li>
+				<li>
+					<a href="#">privacy</a>
+				</li>
+			</ul>
+		</footer>
 	</body>
 </html>
