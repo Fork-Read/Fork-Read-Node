92b795b19973af741bd221651a83040f3bdacd90
Prateek Agarwal <prateekagr98@gmail.com>
2014-07-03 21:39:24 +0530

    Added the UI for the signup page

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,10 +5,10 @@
 		{{ HTML::style('css/reset.css') }}
 		{{ HTML::style('css/login.css') }}
 	</head>
-	<body>
+	<body id="login">
 		<div class="login-form-container">
-			<input type="text" name="username" placeholder="enter email.." autofocus/>
-			<input type="password" name="password" placeholder="enter password.." />
+			<input class="input-box" type="text" name="username" placeholder="enter email.." autofocus/>
+			<input class="input-box" type="password" name="password" placeholder="enter password.." />
 			<button type="submit" class="btn login-button">Login</button>
 			<button type="button" class="btn signup-button" onclick="location.href = '/signup';">Signup</button>
 		</div>
