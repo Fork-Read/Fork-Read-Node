660d81f191b008933ee57fe0f8d0745584e522cf
Prateek Agarwal <prateekagr98@gmail.com>
2014-08-18 22:30:25 +0530

    Add to owned book API

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -7,54 +7,66 @@
 		}
 
 		public function booksOwned(){
-			
-			return Input::all();
 
 			$input = Input::all();
 
-			$book_input = $input['book'];
-			$author_input = $input['author'];
+			$book_array = $input['books_list'];
 			$user_id = $input['user'];
 
-			if(!$book_input || !$author_input || !$user_id){     // Return false if any of the values is not provided
+			if(!$book_array || !$user_id){     // Return false if any of the values is not provided
 				return "false";
 			}
 
-			$book = Book::whereIsbn($book_input['isbn'])->get();
-			$author = Author::whereName($author_input['name'])->get();
 
 			$user = User::whereId($user_id)->get();
 
-			if($user->isEmpty()){		// If User doesn't exist then return false
+			if($user->isEmpty()) {		// If User doesn't exist then return false
 				return "false";
 			}
-
-			$user = $user->first();
-
-			if($author->isEmpty()){      // Check if the author is already present in the database
-				$author = new Author;
-				$author->name = $author_input['name'];
-				$author->save();
+			else {
+				$user = $user->first();
 			}
 
-			if($book->isEmpty()){       // Check if the current book is already in the database
-				$book = new Book;
-				$book->title = $book_input['title'];
-				$book->isbn = $book_input['isbn'];
-				$book->category = $book_input['category'];
-				$book->publisher = $book_input['publisher'];
-				$book->published_date = $book_input['publishedDate'];
-				$book->description = $book_input['description'];
-				$book->thumbnail = $book_input['thumbnail'];
-				$book->save();
-				$book->authors()->save($author);
+			foreach($book_array as $item) {
+
+				$book_item = $item['book'];
+    			
+				$book = Book::whereIsbn($book_item['isbn'])->get();
+
+				if($book->isEmpty()){       // Check if the current book is already in the database
+					$book = new Book;
+					$book->title = $book_item['title'];
+					$book->isbn = $book_item['isbn'];
+					$book->category = $book_item['category'];
+					$book->publisher = $book_item['publisher'];
+					$book->published_date = $book_item['publishedDate'];
+					$book->description = $book_item['description'];
+					$book->thumbnail = $book_item['thumbnail'];
+					$book->save();
+				}
+				else {
+					$book = $book->first();
+				}
+
+				$author_array = $item['authors'];
+
+				foreach($author_array as $i) {
+    				$author = Author::whereName($i['name'])->get();
+
+    				if($author->isEmpty()){      // Check if the author is already present in the database
+						$author = new Author;
+						$author->name = $i['name'];
+						$author->save();
+					}
+					else {
+						$author = $author->first();
+					}
+					$author->books()->save($book);
+				}
+
+				$book->users()->save($user);    // Save entry to owned book list
 			}
 
-			$book = $book->first();
-
-			$book->users()->save($user);    // Save entry to owned book list
-
-			return $book; 
-			
+			return "true"; 		
 		}
 	}
