3c260cc66e328cb59b5cd3abe3734fdac94b5093
Prateek Agarwal <prateekagr98@gmail.com>
2015-01-02 23:44:43 +0530

    Admin Login Functionality

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require ('http');             // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
+
+var 
+    express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    http = require ('http'),             // For serving a basic web page.
+    mongoose = require ("mongoose"),
+    passport = require('passport'),
+    session = require('express-session'),
+    LocalStrategy = require('passport-local').Strategy,
+    AdminModel = require('./models/AdminModel');
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost:27017/snickers';
+var uristring = process.env.MONGOLAB_URI ||
+                process.env.MONGOHQ_URL ||
+                'mongodb://localhost:27017/snickers';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
@@ -21,17 +26,44 @@ var theport = process.env.PORT || 5000;
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
-  if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log ('Succeeded connected to: ' + uristring);
-  }
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
 });
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var books = require('./routes/books');
-var admin = require('./routes/admin');
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        AdminModel.findOne({ username: username }, function(err, admin) {
+            if (err) { return done(err); }
+
+            if (!admin) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!admin.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, admin);
+        });
+    }
+));
+
+passport.serializeUser(function(admin, done) {
+  done(null, admin._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  AdminModel.findById(id, function(err, admin) {
+    done(err, admin);
+  });
+});
+
+var 
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
+    books = require('./routes/books'),
+    admin = require('./routes/admin');
 
 var app = express();
 
@@ -46,8 +78,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Define the Routes
+/*app.post('/admin/authenticate',
+  passport.authenticate('local', { successRedirect: '/admin',
+                                   failureRedirect: '/admin/login',
+                                   failureFlash: false })
+);
+*/
 app.use('/', routes);
+app.use('/admin', admin);
 app.use('/api/users', users);
 app.use('/api/books', books);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+function isAuthenticated(req, res, next) {
+
+    // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+    // you can do this however you want with whatever variables you set up
+    if (req.user && req.user._id)
+        return next();
+
+    // IF A USER ISN'T LOGGED IN, THEN REDIRECT THEM SOMEWHERE
+    res.redirect('/admin/login');
+}
 
 /* GET Admin Homepage */
-router.get('/', function(req, res) {
-  res.render('admin-login');
+router.get('/', isAuthenticated, function(req, res) {
+  res.render('admin-index');
 });
+
+router.get('/login', function(req, res) {
+	res.render('admin-login');
+});
+
+router.post('/authenticate', passport.authenticate('local', { 
+	successRedirect: '/admin',
+    failureRedirect: '/admin/login',
+    failureFlash: false })
+);
 
 module.exports = router;
