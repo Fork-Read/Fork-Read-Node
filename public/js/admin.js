c425d98eb8b0328887cc0d46a9649021f1d2f37e
Prateek Agarwal <prateekagr98@gmail.com>
2015-01-13 23:05:31 +0530

    Added Dialog to delete API

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,59 +3,95 @@
 	$(document).ready(function(){
 
 		$('.item-delete').on('click', function(e){
-			var id = $(this).attr('data-user');
-			$.ajax({
-				url: '/admin/user/changeStatus',
-				type: 'POST',
-				data: JSON.stringify({user: id, isActive: false}),
-				contentType: 'application/json',
-				success: function(status){
-					if(status.hasChanged){
-						$(e.target).closest('.user-item').remove();
-					}
-					else{
-						alert('not deleted');
-						// Show Alert
+			var self = this;
+
+			$.prompt('This user would be deactivated. Are you sure?', {
+				title: "Deactivate User",
+				buttons: { "Yes, Deactivate User": true, "No, Lets Wait": false },
+				overlayspeed: "fast",
+				submit: function(e,v,m,f){
+					// use e.preventDefault() to prevent closing when needed or return false. 
+					// e.preventDefault(); 
+					if(v){
+						var id = $(self).attr('data-user');
+						$.ajax({
+							url: '/admin/user/changeStatus',
+							type: 'POST',
+							data: JSON.stringify({user: id, isActive: false}),
+							contentType: 'application/json',
+							success: function(status){
+								if(status.hasChanged){
+									$(self).closest('.user-item').remove();
+								}
+								else{
+									$.prompt("Unable to deactivate User. Try after some time :)");
+								}
+							}
+						});
 					}
 				}
 			});
 		});
 
 		$('.item-activate').on('click', function(e){
-			var id = $(this).attr('data-user');
-			$.ajax({
-				url: '/admin/user/changeStatus',
-				type: 'POST',
-				data: JSON.stringify({user: id, isActive: true}),
-				contentType: 'application/json',
-				success: function(status){
-					if(status.hasChanged){
-						$(e.target).closest('.user-item').remove();
-					}
-					else{
-						alert('not deleted');
-						// Show Alert
+			var self = this;
+
+			$.prompt('This user would be activated. Are you sure?', {
+				title: "Activate User",
+				buttons: { "Yes, Activate User": true, "No, Lets Wait": false },
+				overlayspeed: "fast",
+				submit: function(e,v,m,f){
+					// use e.preventDefault() to prevent closing when needed or return false. 
+					// e.preventDefault(); 
+					if(v){
+						var id = $(self).attr('data-user');
+						$.ajax({
+							url: '/admin/user/changeStatus',
+							type: 'POST',
+							data: JSON.stringify({user: id, isActive: true}),
+							contentType: 'application/json',
+							success: function(status){
+								if(status.hasChanged){
+									$(self).closest('.user-item').remove();
+								}
+								else{
+									$.prompt("Unable to activate User. Try after some time :)");
+								}
+							}
+						});
 					}
 				}
 			});
 		});
 
 		$('.item-disown').on('click', function(e){
-			var user = $(this).attr('data-user'),
-				book = $(this).attr('data-book');
+			var self = this;
 
-			$.ajax({
-				url: '/api/books/disown',
-				type: 'POST',
-				data: JSON.stringify({user: user, book: book}),
-				contentType: 'application/json',
-				success: function(status){
-					if(status){
-						$(e.target).closest('.book-item').remove();
-					}
-					else{
-						alert('not deleted');
-						// Show Alert
+			$.prompt('This book will be removed from users owned list. Are you sure?', {
+				title: "Disown Books",
+				buttons: { "Yes, Disown Book": true, "No, Lets Wait": false },
+				overlayspeed: "fast",
+				submit: function(e,v,m,f){
+					// use e.preventDefault() to prevent closing when needed or return false. 
+					// e.preventDefault(); 
+					if(v){
+						var user = $(self).attr('data-user'),
+							book = $(self).attr('data-book');
+
+						$.ajax({
+							url: '/api/books/disown',
+							type: 'POST',
+							data: JSON.stringify({user: user, book: book}),
+							contentType: 'application/json',
+							success: function(status){
+								if(status){
+									$(self).closest('.book-item').remove();
+								}
+								else{
+									$.prompt("Unable to disown the book. Try after some time :)");
+								}
+							}
+						});
 					}
 				}
 			});
