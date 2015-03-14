e0a5bd23f0c84d4529fc9a7b2fbcda17ce937fc7
Prateek Agarwal <prateekagr98@gmail.com>
2015-03-14 15:44:09 +0530

    Save Device ID when user logs in

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,36 @@
-var 
-	express = require('express'),
-	router = express.Router(),
-	UserModel = require('../models/UserModel');
+var
+    express = require('express'),
+    router = express.Router(),
+    gcm = require('node-gcm'),
+    UserModel = require('../models/UserModel');
 
-router.get('/:email', function(req, res) {
-	var email = req.param('email');
+router.get('/:email', function (req, res) {
+    var email = req.param('email');
 
-	if(email) {
+    if (email) {
 
-		UserModel.findOne({'email': email}, function(err, user) {
-			if(err) {
-				return console.error(err);
-			}
-			if(user){
-				res.set('Content-Type', 'application/json');
-				res.send(JSON.stringify(user));
-			}
-			else{
-				res.set('Content-Type', 'application/json');
-				res.send(JSON.stringify({}));
-			}
-		});
-	}
-	else{
-		res.redirect('/noResult');
-	}
+        UserModel.findOne({
+            'email': email
+        }, function (err, user) {
+            if (err) {
+                return console.error(err);
+            }
+            if (user) {
+                res.set('Content-Type', 'application/json');
+                res.send(JSON.stringify(user));
+            } else {
+                res.set('Content-Type', 'application/json');
+                res.send(JSON.stringify({}));
+            }
+        });
+    } else {
+        res.redirect('/noResult');
+    }
 });
 
-router.post('/save', function(req, res) {
+router.post('/save', function (req, res) {
 
-	/*
+    /*
 	Expected Object Structure
 	{
 	  "name": "Prateek Agarwal",
@@ -57,65 +58,95 @@ router.post('/save', function(req, res) {
 	  "searchHistory": []
 	}*/
 
-	var email = req.body.email;
+    var email = req.body.email;
 
-	if(email) {
-		UserModel.findOne({email: email}, function(err, user) {
+    if (email) {
+        UserModel.findOne({
+            email: email
+        }, function (err, user) {
 
-			if(err) {
-				return console.error(err);
-			}
+            if (err) {
+                return console.error(err);
+            }
 
-			if(user) {
-				res.set('Content-Type', 'application/json');
-				res.send(JSON.stringify(user));
-			}
-			else {
-				var newUser = new UserModel({
-					name: req.body.name,
-					email: req.body.email,
-					pictureUrl: req.body.pictureUrl,
-					contactNo: req.body.contactNo,
-					gender: req.body.gender,
-					currentLocation: req.body.currentLocation,
-					books: [],   // No Books will be added to owned list when user entry is created,
-					searchHistory: [],  // No Searched Locations will be added when user entry is created
-					isActive: true
-				});
+            if (user) {
+                // Add Device ID if not already present
+                if (user.devices.indexOf(req.body.device) === -1) {
+                    user.devices.push(req.body.devices);
+                    UserModel.findOneAndUpdate({
+                        email: email
+                    }, {
+                        devices: user.devices
+                    }, function (err, user) {
+                        if (err) {
+                            return console.error(err);
+                        }
 
-				newUser.save(function(err, newUser) {
-					if(err) {
-						return console.error(err);
-					}
+                        res.set('Content-Type', 'application/json');
+                        res.send(JSON.stringify(user));
+                    });
+                } else {
+                    // If already present then send the user object back
+                    res.set('Content-Type', 'application/json');
+                    res.send(JSON.stringify(user));
+                }
+            } else {
+                var newUser = new UserModel({
+                    name: req.body.name,
+                    email: req.body.email,
+                    pictureUrl: req.body.pictureUrl,
+                    contactNo: req.body.contactNo,
+                    gender: req.body.gender,
+                    currentLocation: req.body.currentLocation,
+                    books: [], // No Books will be added to owned list when user entry is created,
+                    searchHistory: [], // No Searched Locations will be added when user entry is created
+                    isActive: true,
+                    devices: [req.body.device]
+                });
 
-					res.set('Content-Type', 'application/json');
-					res.send(JSON.stringify(newUser));
-				});
-			}
-		});
-	}
-	else {
-		res.redirect('/noResult');
-	}
+                newUser.save(function (err, newUser) {
+                    if (err) {
+                        return console.error(err);
+                    }
+
+                    res.set('Content-Type', 'application/json');
+                    res.send(JSON.stringify(newUser));
+                });
+            }
+        });
+    } else {
+        res.redirect('/noResult');
+    }
 });
 
-router.post('/update', function(req, res) {
-	var email = req.body.email;
+router.post('/update', function (req, res) {
+    var email = req.body.email;
 
-	if(email) {
-		UserModel.findOneAndUpdate({email: email}, {currentLocation: req.body.currentLocation}, function(err, user) {
-			if(err) {
-				return console.error(err);
-			}
+    if (email) {
+        UserModel.findOneAndUpdate({
+            email: email
+        }, {
+            currentLocation: req.body.currentLocation
+        }, function (err, user) {
+            if (err) {
+                return console.error(err);
+            }
 
-			res.set('Content-Type', 'application/json');
-			res.send(JSON.stringify(user));
-			
-		});
-	}
-	else{
-		res.redirect('/noResult');
-	}
+            res.set('Content-Type', 'application/json');
+            res.send(JSON.stringify(user));
+
+        });
+    } else {
+        res.redirect('/noResult');
+    }
+});
+
+router.post('/sendMessage', function (req, res) {
+    var message = new gcm.Message(),
+        sender = gcm.Sender('AIzaSyARi8rrbEO7Exv3WlB2ozDbKxGViR8uBRo'),
+        devices = [];
+
+    message.addDate('notification', 'This is a notification message');
 });
 
 module.exports = router;
