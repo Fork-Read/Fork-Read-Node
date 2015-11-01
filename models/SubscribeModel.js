4a144313dc54e2c9b3da3cb8a701494a3bb6c226
Prateek Agarwal <prateekagr98@gmail.com>
2015-11-01 19:00:07 +0530

    Remove unused models

--- models/SubscribeModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var subscribeSchema = mongoose.Schema({
-    email: String
-});
-
-var SubscribeModel = mongoose.model('Subscribe', subscribeSchema);
-
-module.exports = SubscribeModel;
